666e44ea7c7e1712fcd0ccd6cfce79e72e8eac62
Anguelos Nicolaou <anguelos.nicolaou@gmail.com>
2023-03-09 01:12:13 +0100

    added formats

--- frat/formats.py
+++ frat/formats.py
@@ -1,12 +1,61 @@
 import numpy as np
+from typing import Union, List, Tuple
 
 
-def rectangles_to_rgb(rect_list, rect_classes, output_size, multiclass=True, color_palete=[]):
+def htmlrgb_to_uint8(color:Union[Tuple[str], List[str], str]):
+    if isinstance(color, str):
+        if "," in color:
+            colors = tuple(color.split(","))
+        else:
+            colors = (color,)
+    else:
+        assert isinstance(color, list) or isinstance(color, tuple)
+        colors = tuple(color)
+    res = np.zeros([len(colors), 3], dtype=np.uint8)
+    for n, color_str in enumerate(colors):
+        color_str = color_str.lower()
+        res[n, :] = [eval(f"0x{color_str[start: end]}") for start, end  in [(1,3), (3,5), (5,7)]]
+    return res.tolist()
+
+
+def rectangles_to_rgb(rect_list, rect_classes, output_size, color_palete, multiclass=False):
+    if multiclass:
+        return rectangles_to_rgb_multiclass(rect_list=rect_list, rect_classes=rect_classes,output_size=output_size, color_palete=color_palete)
+    else:
+        return rectangles_to_rgb_singleclass(rect_list=rect_list, rect_classes=rect_classes,output_size=output_size, color_palete=color_palete)
+    
+
+def rectangles_to_rgb_singleclass(rect_list, rect_classes, output_size, color_palete=[]):
+    class_images = []
+    output_size = output_size[1], output_size[0] # WxH to HxW
+    integral_rgb = np.zeros(tuple(output_size)+(3,), dtype=np.int32)
+    coverage_image = np.zeros([output_size[0], output_size[1]], dtype=np.int32)
+    for classid in sorted(set(rect_classes)):
+        #rgb = [classid%256, (classid//256)%256, (classid//65536)%256]
+        #color_img = np.zeros([output_size[1], output_size[0]], 3, dtype=np.int32)
+        rects = [rect for rect, rect_class in zip(rect_list,rect_classes) if classid==rect_class]
+        for l,t,r,b in rects:
+            print(f"l:{l}, t:{t}, r:{r}, b:{b} , img={output_size}")
+            integral_rgb[t,l,:] += color_palete[classid]
+            integral_rgb[b,r,:] += color_palete[classid]
+            integral_rgb[t,r,:] -= color_palete[classid]
+            integral_rgb[b,l,:] -= color_palete[classid]
+            coverage_image[t,l] += 1
+            coverage_image[b,r] += 1
+            coverage_image[t,r] -= 1
+            coverage_image[b,l] -= 1
+    rgb = (integral_rgb.cumsum(axis=0).cumsum(axis=1)//coverage_image.cumsum(axis=0).cumsum(axis=1)[:,:,None])
+    return rgb[:,:,[2,1,0]].astype(np.uint8)
+
+
+def rectangles_to_rgb_multiclass(rect_list, rect_classes, output_size, color_palete):
+    raise NotImplementedError
     class_images = []
     use_palete = color_palete is not None and len(color_palete) > 0 
     if use_palete:
         coverage_image = np.zeros([output_size[1], output_size[0]], dtype=np.int32)
     for classid in sorted(set(rect_classes)):
+        #rgb = [classid%256, (classid//256)%256, (classid//65536)%256]
         if multiclass:
             class_val = 2**classid
         elif len(color_palete) > 0:
@@ -17,7 +66,7 @@ def rectangles_to_rgb(rect_list, rect_classes, output_size, multiclass=True, col
         class_img = np.zeros([output_size[1], output_size[0]], dtype=np.int32)
         rects = [rect for rect, rect_class in zip(rect_list,rect_classes) if classid==rect_class]
         if len(rects):
-            print("I:", rgb, len(rects), "#{:06x}".format(class_val))
+            #print("I:", rgb, len(rects), "#{:06x}".format(class_val))
             print([class_val%256, (class_val//256)%256, (class_val//65536)%256])
         for l,t,r,b in rects:
             class_img[t:b,l:r]=class_val
